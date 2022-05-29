3166476f511d50e1aac05f5fa89102430a0135fe
khalil <khalillakhdharatc@gmail.com>
2022-05-29 16:11:36 +0100

    interfaçage

---
+++ src/interfacage/CalculerFin.java
@@ -0,0 +1,23 @@
+package interfacage;
+
+public class CalculerFin implements CalculeInterface {
+
+	@Override
+	public int somme() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int produit() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void divise() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
